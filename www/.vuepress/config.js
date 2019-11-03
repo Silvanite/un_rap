ede058a1e070fbf26d7fe71b38cbfb5c0f39168f
Marco Mark <m2de@outlook.com>
2019-11-03 22:10:40 +0000

    Attempt to improve SEO rankings by capturing alternative spellings

--- www/.vuepress/config.js
+++ www/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'un_rap',
+    title: 'un_rap (un rap, unwrap, un-rap) Falmouth, Cornwall',
     description: 'Falmouth’s first & only zero waste shop - designed to reduce impact on our planet & benefit the local community. Refill your containers with sustainable, eco-friendly products. From food & cleaning products, to lifestyle goods & toiletries.',
     head: [
         ['script', {
